a50f3f306c55bcb515baac6d97fe05b06ca860b4
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-12 18:19:33 -0300

    feat: user model

---
+++ src/core/domain/models/user/mock.ts
@@ -0,0 +1,9 @@
+import { User } from '.'
+import { faker } from '@faker-js/faker'
+
+export const mockUser = (): User => ({
+  id: faker.datatype.uuid(),
+  name: faker.name.fullName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+})
