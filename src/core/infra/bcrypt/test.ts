2be51b091d19df26281c9ee0196d64857fa7d226
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-18 10:41:58 -0300

    test: return a string hashed in BcryptEncrypter

--- src/core/infra/bcrypt/test.ts
+++ src/core/infra/bcrypt/test.ts
@@ -3,10 +3,19 @@ import * as bcrypt from 'bcrypt'
 import { EncrypterParams } from 'core/app/protocols/encrypter'
 import { faker } from '@faker-js/faker'
 
+const makeSut = () => {
+  const hashSpy = jest.spyOn(bcrypt, 'hash')
+  const sut = new BcryptEncrypter()
+
+  return {
+    hashSpy,
+    sut,
+  }
+}
+
 describe('BcryptEncrypter', () => {
   it('should call bcrypt hash with right params', async () => {
-    const hashSpy = jest.spyOn(bcrypt, 'hash')
-    const sut = new BcryptEncrypter()
+    const { sut, hashSpy } = makeSut()
 
     const params: EncrypterParams = {
       value: faker.internet.password(),
@@ -14,5 +23,19 @@ describe('BcryptEncrypter', () => {
     await sut.encrypt(params)
 
     expect(hashSpy).toBeCalledWith(params.value, 10)
+  })
+
+  it('should return a string hashed', async () => {
+    const { sut, hashSpy } = makeSut()
+
+    const expectedHash = faker.datatype.uuid()
+    hashSpy.mockImplementationOnce(() => Promise.resolve(expectedHash))
+
+    const params: EncrypterParams = {
+      value: faker.internet.password(),
+    }
+    const hash = await sut.encrypt(params)
+
+    expect(hash).toBe(expectedHash)
   })
 })
