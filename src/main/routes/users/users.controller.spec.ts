408762a4c4908c2cfb230de28f43c8f0d0324935
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-19 10:36:51 -0300

    test: response with right data after creation in UsersController

--- src/main/routes/users/users.controller.spec.ts
+++ src/main/routes/users/users.controller.spec.ts
@@ -3,6 +3,16 @@ import { mockUserCreaterParams } from 'domain/use-cases/user-creater/mock'
 import { mockUsersModule } from './factory.module'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
+import { createMock } from '@golevelup/ts-jest'
+import { Response } from 'express'
+import { HttpResponse, HttpStatusCode } from 'presentation/protocols/http'
+import { mockUser } from 'domain/models/user/mock'
+
+const mockResponse = () =>
+  createMock<Response>({
+    status: jest.fn().mockReturnThis(),
+    send: jest.fn(),
+  })
 
 describe('UsersController', () => {
   let controller: UsersController
@@ -24,9 +34,26 @@ describe('UsersController', () => {
   it('should call create user with right params', async () => {
     const createSpy = jest.spyOn(userService, 'create')
 
+    const res = mockResponse()
     const params = mockUserCreaterParams()
-    await controller.create(params)
+    await controller.create(params, res)
 
     expect(createSpy).toBeCalledWith(params)
+  })
+
+  it('should response with right data after creation', async () => {
+    const createSpy = jest.spyOn(userService, 'create')
+    const result: HttpResponse = {
+      data: mockUser(),
+      statusCode: HttpStatusCode.CREATED,
+    }
+    createSpy.mockResolvedValueOnce(result)
+
+    const res = mockResponse()
+    const params = mockUserCreaterParams()
+    await controller.create(params, res)
+
+    expect(res.status).toBeCalledWith(result.statusCode)
+    expect(res.send).toBeCalledWith({ data: result.data })
   })
 })
