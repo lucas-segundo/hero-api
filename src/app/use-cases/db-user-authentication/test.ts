631f9d00a1826df1459667cd38a425c1fc278d14
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-23 16:05:00 -0300

    test: throw error if hash comparer return false in DbUserAuthentication

--- src/app/use-cases/db-user-authentication/test.ts
+++ src/app/use-cases/db-user-authentication/test.ts
@@ -1,4 +1,5 @@
 import { DataNotFoundError } from 'app/errors/data-not-found-error'
+import { WrongPasswordError } from 'app/errors/wrong-password-error'
 import { HashComparerParams } from 'app/protocols/hash-comparer'
 import { mockHashComparer } from 'app/protocols/hash-comparer/mock'
 import { UserFinderRepositoryParams } from 'app/protocols/user-finder-repository'
@@ -21,8 +22,10 @@ const makeSut = () => {
 
 describe('DbUserAuthentication', () => {
   it('should call user finder with right params', async () => {
-    const { sut, userFinderRepository } = makeSut()
+    const { sut, userFinderRepository, hashComparer } = makeSut()
+
     userFinderRepository.find.mockResolvedValueOnce(mockUser())
+    hashComparer.compare.mockResolvedValueOnce(true)
 
     const params = mockUserAuthenticationParams()
     await sut.auth(params)
@@ -47,7 +50,9 @@ describe('DbUserAuthentication', () => {
   it('should call hash comparer with right params', async () => {
     const { sut, userFinderRepository, hashComparer } = makeSut()
     const user = mockUser()
+
     userFinderRepository.find.mockResolvedValueOnce(user)
+    hashComparer.compare.mockResolvedValueOnce(true)
 
     const params = mockUserAuthenticationParams()
     await sut.auth(params)
@@ -57,5 +62,18 @@ describe('DbUserAuthentication', () => {
       value: params.password,
     }
     expect(hashComparer.compare).toBeCalledWith(compareParams)
+  })
+
+  it('should throw error if hash comparer return false', async () => {
+    const { sut, userFinderRepository, hashComparer } = makeSut()
+    const user = mockUser()
+
+    userFinderRepository.find.mockResolvedValueOnce(user)
+    hashComparer.compare.mockResolvedValueOnce(false)
+
+    const params = mockUserAuthenticationParams()
+    const result = sut.auth(params)
+
+    await expect(result).rejects.toThrowError(new WrongPasswordError())
   })
 })
