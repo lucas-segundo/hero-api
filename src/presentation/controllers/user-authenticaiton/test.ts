31682d886947921263b086dcc31bbd18eef71cda
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-24 14:38:50 -0300

    test: return errors if request is missing the required data in UserAuthenticationController

--- src/presentation/controllers/user-authenticaiton/test.ts
+++ src/presentation/controllers/user-authenticaiton/test.ts
@@ -1,6 +1,8 @@
 import { faker } from '@faker-js/faker'
 import { UserAuthenticationParams } from 'domain/use-cases/user-authentication'
 import { mockUserAuthentication } from 'domain/use-cases/user-authentication/mock'
+import { MissingParamError } from 'presentation/errors/missing-param-error'
+import { HttpErrorResponse, HttpStatusCode } from 'presentation/protocols/http'
 import { UserAuthenticationController } from '.'
 
 describe('UserAuthenticationController', () => {
@@ -16,5 +18,25 @@ describe('UserAuthenticationController', () => {
     await sut.handle(params)
 
     expect(userAuthentication.auth).toBeCalledWith(params)
+  })
+
+  it('should return errors if request is missing the required data', async () => {
+    const userAuthentication = mockUserAuthentication()
+    const sut = new UserAuthenticationController(userAuthentication)
+
+    const params: UserAuthenticationParams = {
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    }
+
+    delete params.password
+    const response = await sut.handle(params)
+
+    const expectedResponse: HttpErrorResponse = {
+      errors: [new MissingParamError('password').message],
+      statusCode: HttpStatusCode.BAD_REQUEST,
+    }
+
+    expect(response).toEqual(expectedResponse)
   })
 })
