74e254f37d1f4b4fee095494879d64577834c19d
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-24 18:51:36 -0300

    test: return a user in KnexUserFinderRepository

--- src/infra/knex/repositories/user-finder/test.ts
+++ src/infra/knex/repositories/user-finder/test.ts
@@ -1,13 +1,34 @@
 import { faker } from '@faker-js/faker'
+import { DbUser } from 'app/models/db-user'
+import { mockDbUser } from 'app/models/db-user/mock'
 import { UserFinderRepositoryParams } from 'app/protocols/user-finder-repository'
 import { KnexDbHandler } from 'infra/knex/config/knex-db-handler'
 import { Knex } from 'knex'
 import { KnexUserFinderRepository } from '.'
 
+const mockUser = () => {
+  const sut = new KnexUserFinderRepository(KnexDbHandler.client)
+
+  return {
+    sut,
+  }
+}
+
 describe('KnexUserFinderRepository', () => {
+  let expectedDbUser: DbUser
+
   beforeAll(async () => {
     KnexDbHandler.connect('test')
     await KnexDbHandler.migrateLatest()
+
+    const userToInsert = mockDbUser()
+    delete userToInsert.id
+
+    const userId = await KnexDbHandler.client.insert(userToInsert).into('users')
+    expectedDbUser = {
+      id: userId[0],
+      ...userToInsert,
+    }
   })
 
   afterAll(async () => {
@@ -15,13 +36,14 @@ describe('KnexUserFinderRepository', () => {
   })
 
   it('should call client with right params', async () => {
-    const client: Partial<jest.Mocked<Knex>> = {
+    const client = {
       select: jest.fn().mockReturnThis(),
       from: jest.fn().mockReturnThis(),
       where: jest.fn().mockReturnThis(),
       first: jest.fn().mockReturnThis(),
-    }
-    const sut = new KnexUserFinderRepository(client as unknown as Knex)
+    } as unknown as Knex
+
+    const sut = new KnexUserFinderRepository(client)
 
     const params: UserFinderRepositoryParams = {
       by: 'email',
@@ -34,5 +56,18 @@ describe('KnexUserFinderRepository', () => {
     expect(client.from).toBeCalledWith('users')
     expect(client.where).toBeCalledWith(params.by, params.value)
     expect(client.first).toBeCalled()
+  })
+
+  it('should return a user', async () => {
+    const { sut } = mockUser()
+
+    const params: UserFinderRepositoryParams = {
+      by: 'email',
+      value: expectedDbUser.email,
+    }
+
+    const user = await sut.find(params)
+
+    expect(user).toEqual(expectedDbUser)
   })
 })
