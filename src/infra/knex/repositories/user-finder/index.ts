8419b5f1b95031962c595869b5d58aadd450d22a
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-24 18:51:05 -0300

    feat: return a user in KnexUserFinderRepository

--- src/infra/knex/repositories/user-finder/index.ts
+++ src/infra/knex/repositories/user-finder/index.ts
@@ -8,8 +8,12 @@ import { Knex } from 'knex'
 export class KnexUserFinderRepository implements UserFinderRepository {
   constructor(private client: Knex) {}
   async find({ by, value }: UserFinderRepositoryParams): Promise<DbUser> {
-    await this.client.select('*').from('users').where(by, value).first()
+    const data = await this.client
+      .select('*')
+      .from<DbUser>('users')
+      .where(by, value)
+      .first()
 
-    return null
+    return data
   }
 }
