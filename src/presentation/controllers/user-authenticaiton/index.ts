1b75e5e582c9560afd68e3a44d23f15dd977e3eb
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-24 14:37:52 -0300

    feat: return errors if request is missing the required data in UserAuthenticationController

--- src/presentation/controllers/user-authenticaiton/index.ts
+++ src/presentation/controllers/user-authenticaiton/index.ts
@@ -2,8 +2,13 @@ import {
   UserAuthentication,
   UserAuthenticationParams,
 } from 'domain/use-cases/user-authentication'
+import { MissingParamError } from 'presentation/errors/missing-param-error'
 import { Controller } from 'presentation/protocols/controller'
-import { HttpResponse, HttpErrorResponse } from 'presentation/protocols/http'
+import {
+  HttpResponse,
+  HttpErrorResponse,
+  HttpStatusCode,
+} from 'presentation/protocols/http'
 
 export class UserAuthenticationController implements Controller {
   constructor(private userAuthentication: UserAuthentication) {}
@@ -11,6 +16,19 @@ export class UserAuthenticationController implements Controller {
   async handle(
     params: UserAuthenticationParams
   ): Promise<HttpResponse | HttpErrorResponse> {
+    const errors = []
+
+    const { email, password } = params
+    !email && errors.push(new MissingParamError('email').message)
+    !password && errors.push(new MissingParamError('password').message)
+
+    if (errors.length) {
+      return {
+        errors,
+        statusCode: HttpStatusCode.BAD_REQUEST,
+      }
+    }
+
     await this.userAuthentication.auth(params)
     return null
   }
