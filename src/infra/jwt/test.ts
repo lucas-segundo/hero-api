abc9131896ae75395b571f58180396673d4a08fe
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-25 10:58:57 -0300

    test: return jwt token in JwtEncrypter

--- src/infra/jwt/test.ts
+++ src/infra/jwt/test.ts
@@ -3,13 +3,22 @@ import { JwtEncrypter } from '.'
 import * as jwt from 'jsonwebtoken'
 import { mockEncrypterParams } from 'app/protocols/encrypter/mock'
 
+const makeSut = () => {
+  const sut = new JwtEncrypter()
+  const signSpy = jest.spyOn(jwt, 'sign')
+
+  return {
+    sut,
+    signSpy,
+  }
+}
+
 describe('JwtEncrypter', () => {
   process.env.JWT_SECRET = faker.random.word()
 
   it('should call jwt with right params', async () => {
-    const sut = new JwtEncrypter()
+    const { sut, signSpy } = makeSut()
 
-    const signSpy = jest.spyOn(jwt, 'sign')
     const params = mockEncrypterParams()
 
     await sut.encrypt(params)
@@ -22,5 +31,17 @@ describe('JwtEncrypter', () => {
       process.env.JWT_SECRET,
       options
     )
+  })
+
+  it('should return jwt token', async () => {
+    const { sut, signSpy } = makeSut()
+
+    const expectedToken = faker.datatype.uuid()
+    signSpy.mockImplementationOnce(() => expectedToken)
+    const params = mockEncrypterParams()
+
+    const token = await sut.encrypt(params)
+
+    expect(token).toBe(expectedToken)
   })
 })
