cf5d54bbcb52cc353061073246b3d3a961875ad8
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-25 15:33:43 -0300

    test: respond with right data in AuthController

--- src/main/routes/auth/auth.controller.spec.ts
+++ src/main/routes/auth/auth.controller.spec.ts
@@ -1,5 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { mockUserAuthenticationParams } from 'domain/use-cases/user-authentication/mock'
+import { AuthenticatedUser } from 'domain/use-cases/user-authentication'
+import {
+  mockAuthenticatedUser,
+  mockUserAuthenticationParams,
+} from 'domain/use-cases/user-authentication/mock'
+import { mockExpressResponse } from 'main/helpers/mock-express-response'
+import { HttpResponse, HttpStatusCode } from 'presentation/protocols/http'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { mockUserAuthModule } from './mock.module'
@@ -23,10 +29,28 @@ describe('AuthController', () => {
 
   it('should call auth with right params', async () => {
     const serviceAuthSpy = jest.spyOn(service, 'auth')
+    const res = mockExpressResponse()
 
     const authDto = mockUserAuthenticationParams()
-    await controller.auth(authDto)
+    await controller.auth(authDto, res)
 
     expect(serviceAuthSpy).toBeCalledWith(authDto)
+  })
+
+  it('should respond with right data', async () => {
+    const serviceAuthSpy = jest.spyOn(service, 'auth')
+    const res = mockExpressResponse()
+
+    const response: HttpResponse<AuthenticatedUser> = {
+      data: mockAuthenticatedUser(),
+      statusCode: HttpStatusCode.OK,
+    }
+    serviceAuthSpy.mockResolvedValueOnce(response)
+
+    const authDto = mockUserAuthenticationParams()
+    await controller.auth(authDto, res)
+
+    expect(res.status).toBeCalledWith(response.statusCode)
+    expect(res.send).toBeCalledWith({ data: response.data })
   })
 })
