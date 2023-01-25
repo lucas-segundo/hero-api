ea990f6d8c8ef9c290f717602e7be271460a4f41
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-25 10:58:30 -0300

    feat: return jwt token in JwtEncrypter

--- src/infra/jwt/index.ts
+++ src/infra/jwt/index.ts
@@ -3,10 +3,10 @@ import * as jwt from 'jsonwebtoken'
 
 export class JwtEncrypter implements Encrypter {
   async encrypt({ payload }: EncrypterParams): Promise<string> {
-    jwt.sign(payload, process.env.JWT_SECRET, {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
       expiresIn: 60 * 60,
     })
 
-    return null
+    return token
   }
 }
