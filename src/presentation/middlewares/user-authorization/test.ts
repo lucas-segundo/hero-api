38d2743bd0cdfdbfd95d8319c149f1d0b57afae2
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-26 18:31:55 -0300

    test: respond with error if authorization returns false in UserAuthorizationMiddleware

--- src/presentation/middlewares/user-authorization/test.ts
+++ src/presentation/middlewares/user-authorization/test.ts
@@ -2,6 +2,8 @@ import {
   mockUserAuthorization,
   mockUserAuthorizationParams,
 } from 'domain/use-cases/user-authorization/mock'
+import { UnauthorizedError } from 'presentation/errors/unauthorized-error'
+import { HttpErrorResponse } from 'presentation/protocols/http'
 import { UserAuthorizationMiddleware } from '.'
 
 describe('UserAuthorizationMiddleware', () => {
@@ -13,5 +15,22 @@ describe('UserAuthorizationMiddleware', () => {
     await sut.handle(params)
 
     expect(userAuthorization.auth).toBeCalledWith(params)
+  })
+
+  it('should respond with error if authorization returns false', async () => {
+    const userAuthorization = mockUserAuthorization()
+    const sut = new UserAuthorizationMiddleware(userAuthorization)
+
+    userAuthorization.auth.mockResolvedValueOnce(false)
+
+    const params = mockUserAuthorizationParams()
+    const response = await sut.handle(params)
+
+    const error = new UnauthorizedError()
+    const expectedResponse: HttpErrorResponse = {
+      errors: [error.message],
+      statusCode: error.statusCode,
+    }
+    expect(response).toEqual(expectedResponse)
   })
 })
