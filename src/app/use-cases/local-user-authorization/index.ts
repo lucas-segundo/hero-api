be2de6353dcee046dea1ba96e46e316593dd2f3a
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-26 16:32:41 -0300

    feat: call dencrypter with right params in LocalUserAuthorization

---
+++ src/app/use-cases/local-user-authorization/index.ts
@@ -0,0 +1,15 @@
+import { TokenPayload } from 'app/models/token-payload'
+import { Dencrypter } from 'app/protocols/dencrypter'
+import {
+  UserAuthorization,
+  UserAuthorizationParams,
+} from 'domain/use-cases/user-authorization'
+
+export class LocalUserAuthorization implements UserAuthorization {
+  constructor(private dencrypter: Dencrypter) {}
+  async auth(params: UserAuthorizationParams): Promise<boolean> {
+    await this.dencrypter.dencrypt<TokenPayload>(params)
+
+    return null
+  }
+}
