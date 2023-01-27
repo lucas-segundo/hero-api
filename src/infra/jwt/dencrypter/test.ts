232f35d7713af93c1c5dff1cb49cc7c716016565
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-27 09:34:58 -0300

    test: return token decrypted in JwtDencrypter

--- src/infra/jwt/dencrypter/test.ts
+++ src/infra/jwt/dencrypter/test.ts
@@ -3,13 +3,22 @@ import { DencrypterParams } from 'app/protocols/dencrypter'
 import { JwtDencrypter } from '.'
 import * as jwt from 'jsonwebtoken'
 
+const makeSut = () => {
+  const sut = new JwtDencrypter()
+  const verifySpy = jest.spyOn(jwt, 'verify')
+
+  return {
+    sut,
+    verifySpy,
+  }
+}
+
 describe('JwtDencrypter', () => {
   const secret = faker.datatype.uuid()
   process.env.JWT_SECRET = secret
 
   it('should call jwt with right params', async () => {
-    const sut = new JwtDencrypter()
-    const verifySpy = jest.spyOn(jwt, 'verify')
+    const { sut, verifySpy } = makeSut()
     verifySpy.mockReturnValueOnce()
 
     const params: DencrypterParams = {
@@ -18,5 +27,21 @@ describe('JwtDencrypter', () => {
     await sut.dencrypt(params)
 
     expect(verifySpy).toBeCalledWith(params.token, secret)
+  })
+
+  it('should return token decrypted', async () => {
+    const { sut, verifySpy } = makeSut()
+
+    const tokenDecrypted = {
+      [faker.database.column()]: faker.random.word(),
+    }
+    verifySpy.mockImplementationOnce(() => tokenDecrypted)
+
+    const params: DencrypterParams = {
+      token: faker.datatype.uuid(),
+    }
+    const data = await sut.dencrypt(params)
+
+    expect(data).toEqual(tokenDecrypted)
   })
 })
