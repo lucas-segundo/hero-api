b6d7354e45825ad537e0ecf5121d25aa3e9a279d
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-27 09:46:33 -0300

    feat: user auth middleware factory

--- src/main/middlewares/user-authorization.ts
+++ src/main/middlewares/user-authorization.ts
@@ -1,8 +1,10 @@
-// import { LocalUserAuthorization } from 'app/use-cases/local-user-authorization'
-// import { UserAuthorizationMiddleware } from 'presentation/middlewares/user-authorization'
+import { LocalUserAuthorization } from 'app/use-cases/local-user-authorization'
+import { JwtDencrypter } from 'infra/jwt/dencrypter'
+import { UserAuthorizationMiddleware } from 'presentation/middlewares/user-authorization'
 
-// export const makeUserAuthorizationMiddleware = () => {
-//   // const dencrypter = new Bcry
-//   const userAuth = new LocalUserAuthorization()
-//   return new UserAuthorizationMiddleware()
-// }
+export const makeUserAuthorizationMiddleware = () => {
+  const dencrypter = new JwtDencrypter()
+  const userAuth = new LocalUserAuthorization(dencrypter)
+
+  return new UserAuthorizationMiddleware(userAuth)
+}
