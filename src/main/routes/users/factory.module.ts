b9b4db86fd902bc1e8dfbfacdd4b1c692dbc517e
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-27 14:40:13 -0300

    feat: add authorization guard to users route

--- src/main/routes/users/factory.module.ts
+++ src/main/routes/users/factory.module.ts
@@ -1,19 +1,18 @@
-import { ModuleMetadata, Provider } from '@nestjs/common'
+import { ModuleMetadata } from '@nestjs/common'
 import { makeUserCreationController } from 'main/controllers/user-creation-controller-factory'
 import { UserCreationController } from 'presentation/controllers/user-creation'
+import { makeAuthorizationProviders } from '../../middlewares/authorization-guard/providers-factory'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
 
-const customProviders = (): Provider[] => {
-  return [
+export const makeUsersModule = (): ModuleMetadata => ({
+  controllers: [UsersController],
+  providers: [
+    UsersService,
     {
       provide: UserCreationController,
       useFactory: () => makeUserCreationController(),
     },
-  ]
-}
-
-export const makeUsersModule = (): ModuleMetadata => ({
-  controllers: [UsersController],
-  providers: [UsersService, ...customProviders()],
+    ...makeAuthorizationProviders(),
+  ],
 })
--- src/main/routes/users/users.controller.ts
+++ src/main/routes/users/users.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get, Post, Body, Param, Delete, Res } from '@nestjs/common'
+import { Controller, Post, Body, Res, UseGuards } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { Response } from 'express'
 import { handleResponse } from 'main/helpers/handle-response'
-// import { UpdateUserDto } from './dto/update-user.dto'
+import { AuthorizationGuard } from 'main/middlewares/authorization-guard'
 
 @Controller('users')
+@UseGuards(AuthorizationGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
@@ -14,25 +15,5 @@ export class UsersController {
     const result = await this.usersService.create(createUserDto)
 
     handleResponse(res, result)
-  }
-
-  @Get()
-  findAll() {
-    return this.usersService.findAll()
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id)
-  }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-  //   return this.usersService.update(+id, updateUserDto)
-  // }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id)
   }
 }
