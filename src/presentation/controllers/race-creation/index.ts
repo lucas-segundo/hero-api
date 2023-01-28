b94d569ce2daaadf0e4602c21a7fc5ebccb0e3cf
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-28 09:03:00 -0300

    feat: respond with error if it is missing required params in RaceCreationController

--- src/presentation/controllers/race-creation/index.ts
+++ src/presentation/controllers/race-creation/index.ts
@@ -3,6 +3,7 @@ import {
   RaceCreation,
   RaceCreationParams,
 } from 'domain/use-cases/race-creation'
+import { MissingParamError } from 'presentation/errors/missing-param-error'
 import { Controller } from 'presentation/protocols/controller'
 import {
   HttpResponse,
@@ -15,6 +16,16 @@ export class RaceCreationController implements Controller {
   async handle(
     params: RaceCreationParams
   ): Promise<HttpResponse<RaceCreated> | HttpErrorResponse> {
+    const errors = []
+    !params.title && errors.push(new MissingParamError('title'))
+
+    if (errors.length) {
+      return {
+        errors,
+        statusCode: HttpStatusCode.BAD_REQUEST,
+      }
+    }
+
     const userCreated = await this.raceCreation.create(params)
 
     return {
