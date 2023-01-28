2b0806c9fb332c3dfd5a1c83cf3d9dd062403981
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-28 08:52:56 -0300

    test: respond with race createds in RaceCreationController

--- src/domain/use-cases/race-creation/mock.ts
+++ src/domain/use-cases/race-creation/mock.ts
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker'
-import { RaceCreation, RaceCreationParams } from '.'
+import { RaceCreated, RaceCreation, RaceCreationParams } from '.'
 
 export const mockRaceCreation = (): jest.Mocked<RaceCreation> => ({
   create: jest.fn(),
@@ -7,4 +7,8 @@ export const mockRaceCreation = (): jest.Mocked<RaceCreation> => ({
 
 export const mockRaceCreationParams = (): RaceCreationParams => ({
   title: faker.random.word(),
+})
+
+export const mockRaceCreated = (): RaceCreated => ({
+  id: faker.datatype.uuid(),
 })
--- src/presentation/controllers/race-creation/test.ts
+++ src/presentation/controllers/race-creation/test.ts
@@ -1,17 +1,44 @@
 import {
+  mockRaceCreated,
   mockRaceCreation,
   mockRaceCreationParams,
 } from 'domain/use-cases/race-creation/mock'
+import { HttpResponse, HttpStatusCode } from 'presentation/protocols/http'
 import { RaceCreationController } from '.'
+
+const makeSut = () => {
+  const raceCreation = mockRaceCreation()
+  const sut = new RaceCreationController(raceCreation)
+
+  return {
+    raceCreation,
+    sut,
+  }
+}
 
 describe('RaceCreationController', () => {
   it('should call race creation with right params', async () => {
-    const raceCreation = mockRaceCreation()
-    const sut = new RaceCreationController(raceCreation)
+    const { sut, raceCreation } = makeSut()
 
     const params = mockRaceCreationParams()
     await sut.handle(params)
 
     expect(raceCreation.create).toBeCalledWith(params)
+  })
+
+  it('should respond with race created', async () => {
+    const { sut, raceCreation } = makeSut()
+    const raceCreated = mockRaceCreated()
+    raceCreation.create.mockResolvedValueOnce(raceCreated)
+
+    const params = mockRaceCreationParams()
+    const response = await sut.handle(params)
+
+    const expectedResponse: HttpResponse = {
+      data: raceCreated,
+      statusCode: HttpStatusCode.OK,
+    }
+
+    expect(response).toEqual(expectedResponse)
   })
 })
