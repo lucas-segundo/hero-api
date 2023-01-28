4579b74c85f34c8d440cb399891526a9619b7058
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-28 08:52:32 -0300

    feat: respond with race createds in RaceCreationController

--- src/domain/use-cases/race-creation/index.ts
+++ src/domain/use-cases/race-creation/index.ts
@@ -2,6 +2,9 @@ export type RaceCreationParams = {
   title: string
 }
 
+export type RaceCreated = {
+  id: string
+}
 export interface RaceCreation {
-  create(params: RaceCreationParams): Promise<number>
+  create(params: RaceCreationParams): Promise<RaceCreated>
 }
--- src/presentation/controllers/race-creation/index.ts
+++ src/presentation/controllers/race-creation/index.ts
@@ -1,17 +1,25 @@
 import {
+  RaceCreated,
   RaceCreation,
   RaceCreationParams,
 } from 'domain/use-cases/race-creation'
 import { Controller } from 'presentation/protocols/controller'
-import { HttpResponse, HttpErrorResponse } from 'presentation/protocols/http'
+import {
+  HttpResponse,
+  HttpErrorResponse,
+  HttpStatusCode,
+} from 'presentation/protocols/http'
 
 export class RaceCreationController implements Controller {
   constructor(private raceCreation: RaceCreation) {}
   async handle(
     params: RaceCreationParams
-  ): Promise<HttpResponse<any> | HttpErrorResponse> {
-    await this.raceCreation.create(params)
+  ): Promise<HttpResponse<RaceCreated> | HttpErrorResponse> {
+    const userCreated = await this.raceCreation.create(params)
 
-    return
+    return {
+      data: userCreated,
+      statusCode: HttpStatusCode.OK,
+    }
   }
 }
