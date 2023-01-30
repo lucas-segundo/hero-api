5ec08f06c54ac268958c7552f13c41e61fa47788
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-30 09:32:11 -0300

    test: return race created in DbRaceCreation

--- src/app/protocols/race-creation-repository/mock.ts
+++ src/app/protocols/race-creation-repository/mock.ts
@@ -1,4 +1,19 @@
-import { RaceCreationRepository } from '.'
+import { faker } from '@faker-js/faker'
+import {
+  RaceCreationRepository,
+  RaceCreationRepositoryModel,
+  RaceCreationRepositoryParams,
+} from '.'
+
+export const mockRaceCreationRepositoryParams =
+  (): RaceCreationRepositoryParams => ({
+    title: faker.random.words(),
+  })
+
+export const mockRaceCreationRepositoryModel =
+  (): RaceCreationRepositoryModel => ({
+    id: faker.datatype.uuid(),
+  })
 
 export const mockRaceCreationRepository =
   (): jest.Mocked<RaceCreationRepository> => ({
--- src/app/use-cases/db-race-creation/test.ts
+++ src/app/use-cases/db-race-creation/test.ts
@@ -1,15 +1,44 @@
-import { mockRaceCreationRepository } from 'app/protocols/race-creation-repository/mock'
-import { mockRaceCreationParams } from 'domain/use-cases/race-creation/mock'
+import {
+  mockRaceCreationRepository,
+  mockRaceCreationRepositoryModel,
+  mockRaceCreationRepositoryParams,
+} from 'app/protocols/race-creation-repository/mock'
+import { RaceCreated } from 'domain/use-cases/race-creation'
 import { DbRaceCreation } from '.'
+
+const makeSut = () => {
+  const raceCreationRepo = mockRaceCreationRepository()
+  const sut = new DbRaceCreation(raceCreationRepo)
+
+  const params = mockRaceCreationRepositoryParams()
+  const raceCreatedRepo = mockRaceCreationRepositoryModel()
+
+  return {
+    sut,
+    raceCreationRepo,
+    params,
+    raceCreatedRepo,
+  }
+}
 
 describe('DbRaceCreation', () => {
   it('should call race creation repo with right params', async () => {
-    const raceCreationRepo = mockRaceCreationRepository()
-    const sut = new DbRaceCreation(raceCreationRepo)
-
-    const params = mockRaceCreationParams()
+    const { sut, params, raceCreationRepo } = makeSut()
     await sut.create(params)
 
     expect(raceCreationRepo.create).toBeCalledWith(params)
+  })
+
+  it('should return race created', async () => {
+    const { sut, raceCreationRepo, params, raceCreatedRepo } = makeSut()
+
+    raceCreationRepo.create.mockResolvedValueOnce(raceCreatedRepo)
+
+    const data = await sut.create(params)
+
+    const expectedData: RaceCreated = {
+      id: raceCreatedRepo.id,
+    }
+    expect(data).toEqual(expectedData)
   })
 })
