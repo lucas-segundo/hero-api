6409874580d21e66d9bd67f7278933962734c3d6
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-30 15:53:19 -0300

    fix: create table if exist warning

--- src/infra/knex/migrations/20230116192245_create_users_users.ts
+++ src/infra/knex/migrations/20230116192245_create_users_users.ts
@@ -1,12 +1,16 @@
 import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTableIfNotExists('users', (table) => {
-    table.increments('id').primary()
-    table.string('name')
-    table.string('email')
-    table.string('password_hashed')
-  })
+  const hasTable = await knex.schema.hasTable('users')
+
+  if (!hasTable) {
+    return knex.schema.createTableIfNotExists('users', (table) => {
+      table.increments('id').primary()
+      table.string('name')
+      table.string('email')
+      table.string('password_hashed')
+    })
+  }
 }
 
 export async function down(knex: Knex): Promise<void> {
