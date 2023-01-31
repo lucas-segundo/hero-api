ac366bcd0eed3ad10f9deb41b8cfa93aad2829e6
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-01-31 09:37:11 -0300

    test: return model data in KnexRaceCreaterRepository

--- src/infra/knex/repositories/race-creater/test.ts
+++ src/infra/knex/repositories/race-creater/test.ts
@@ -1,7 +1,20 @@
+import { RaceCreaterRepositoryModel } from 'app/protocols/race-creater-repository'
 import { mockRaceCreaterRepositoryParams } from 'app/protocols/race-creater-repository/mock'
 import { KnexDbHandler } from 'infra/knex/config/knex-db-handler'
+import { RacesSchema } from 'infra/knex/schemas/races'
 import { Knex } from 'knex'
 import { KnexRaceCreaterRepository } from '.'
+
+const makeSut = (client = KnexDbHandler.client) => {
+  const sut = new KnexRaceCreaterRepository(client)
+
+  const params = mockRaceCreaterRepositoryParams()
+
+  return {
+    sut,
+    params,
+  }
+}
 
 describe('KnexRaceCreaterRepository', () => {
   beforeAll(async () => {
@@ -18,12 +31,27 @@ describe('KnexRaceCreaterRepository', () => {
       insert: jest.fn().mockReturnThis(),
       into: jest.fn().mockReturnThis(),
     } as unknown as Knex
+    jest.spyOn(client, 'into').mockResolvedValueOnce([{ id: 1 }])
     const sut = new KnexRaceCreaterRepository(client)
 
     const params = mockRaceCreaterRepositoryParams()
     await sut.create(params)
 
-    expect(client.insert).toBeCalledWith(params)
+    expect(client.insert).toBeCalledWith(params, ['*'])
     expect(client.into).toBeCalledWith('races')
+  })
+
+  it('should return model data', async () => {
+    const client = KnexDbHandler.client
+    const { sut, params } = makeSut(client)
+
+    const model = await sut.create(params)
+    const firstRow = await client.select<RacesSchema>('*').from('races').first()
+
+    const expectedModel: RaceCreaterRepositoryModel = {
+      id: firstRow.id.toString(),
+    }
+
+    expect(model).toEqual(expectedModel)
   })
 })
