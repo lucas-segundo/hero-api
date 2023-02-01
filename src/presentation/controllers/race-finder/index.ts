8ceb65ce7af0a10d6b8e4899ae4945c330484e64
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-02-01 16:45:29 -0300

    feat: respond with correct data in RaceFinderController

--- src/presentation/controllers/race-finder/index.ts
+++ src/presentation/controllers/race-finder/index.ts
@@ -1,11 +1,22 @@
 import { Race } from 'domain/models/race'
 import { RaceFinder, RaceFinderParams } from 'domain/use-cases/race-finder'
+import { Controller } from 'presentation/protocols/controller'
+import {
+  HttpResponse,
+  HttpErrorResponse,
+  HttpStatusCode,
+} from 'presentation/protocols/http'
 
-export class RaceFinderController implements RaceFinder {
+export class RaceFinderController implements Controller {
   constructor(private raceFinder: RaceFinder) {}
+  async handle(
+    params: RaceFinderParams
+  ): Promise<HttpResponse<Race> | HttpErrorResponse> {
+    const data = await this.raceFinder.find(params)
 
-  async find(params: RaceFinderParams): Promise<Race> {
-    await this.raceFinder.find(params)
-    return
+    return {
+      data,
+      statusCode: HttpStatusCode.CREATED,
+    }
   }
 }
