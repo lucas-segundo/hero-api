093a2b3a90e3c691ab83e80f3f5e2b2725f12939
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-02-01 17:08:19 -0300

    feat: respond with error if required params are missing in RaceFinderController

--- src/presentation/controllers/race-finder/index.ts
+++ src/presentation/controllers/race-finder/index.ts
@@ -1,5 +1,6 @@
 import { Race } from 'domain/models/race'
 import { RaceFinder, RaceFinderParams } from 'domain/use-cases/race-finder'
+import { MissingParamError } from 'presentation/errors/missing-param-error'
 import { Controller } from 'presentation/protocols/controller'
 import {
   HttpResponse,
@@ -12,6 +13,18 @@ export class RaceFinderController implements Controller {
   async handle(
     params: RaceFinderParams
   ): Promise<HttpResponse<Race> | HttpErrorResponse> {
+    const errors = []
+    if (!params.id) {
+      errors.push(new MissingParamError('id').message)
+    }
+
+    if (errors.length) {
+      return {
+        errors,
+        statusCode: HttpStatusCode.BAD_REQUEST,
+      }
+    }
+
     const data = await this.raceFinder.find(params)
 
     return {
