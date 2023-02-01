c27f3c019a3f517e0724a6b8d4ae9fd99d26dcbb
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-02-01 16:46:17 -0300

    test: respond with correct data in RaceFinderController

--- src/presentation/controllers/race-finder/test.ts
+++ src/presentation/controllers/race-finder/test.ts
@@ -1,8 +1,11 @@
+import { Race } from 'domain/models/race'
+import { mockRace } from 'domain/models/race/mock'
 import { RaceFinderParams } from 'domain/use-cases/race-finder'
 import {
   mockRaceFinder,
   mockRaceFinderParams,
 } from 'domain/use-cases/race-finder/mock'
+import { HttpResponse, HttpStatusCode } from 'presentation/protocols/http'
 import { RaceFinderController } from '.'
 
 describe('RaceFinderController', () => {
@@ -11,8 +14,26 @@ describe('RaceFinderController', () => {
     const sut = new RaceFinderController(raceFinder)
 
     const params: RaceFinderParams = mockRaceFinderParams()
-    await sut.find(params)
+    await sut.handle(params)
 
     expect(raceFinder.find).toBeCalledWith(params)
+  })
+
+  it('should respond with correct data', async () => {
+    const raceFinder = mockRaceFinder()
+    const sut = new RaceFinderController(raceFinder)
+
+    const race = mockRace()
+    raceFinder.find.mockResolvedValueOnce(race)
+
+    const params: RaceFinderParams = mockRaceFinderParams()
+    const data = await sut.handle(params)
+
+    const expectedResponse: HttpResponse<Race> = {
+      data: race,
+      statusCode: HttpStatusCode.CREATED,
+    }
+
+    expect(data).toEqual(expectedResponse)
   })
 })
