28ca3129f15d7384d8aff0cd70c5bddccf97fdfd
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-02-02 10:16:02 -0300

    feat: throw unexpected error if something wrong has happened in DbRaceFinder

--- src/app/use-cases/db-race-finder/index.ts
+++ src/app/use-cases/db-race-finder/index.ts
@@ -1,4 +1,5 @@
 import { RaceFinderRepository } from 'app/protocols/race-finder-repository'
+import { UnexpectedError } from 'domain/errors/unexpected-error'
 import { Race } from 'domain/models/race'
 import { RaceFinder, RaceFinderParams } from 'domain/use-cases/race-finder'
 
@@ -6,11 +7,15 @@ export class DbRaceFinder implements RaceFinder {
   constructor(private raceFinderRepository: RaceFinderRepository) {}
 
   async find(params: RaceFinderParams): Promise<Race> {
-    const { id, title } = await this.raceFinderRepository.find(params)
+    try {
+      const { id, title } = await this.raceFinderRepository.find(params)
 
-    return {
-      id: id.toString(),
-      title,
+      return {
+        id: id.toString(),
+        title,
+      }
+    } catch (error) {
+      throw new UnexpectedError()
     }
   }
 }
