07ecf329465a0bf1e85a041fb85a93149aea18f9
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-02-02 15:01:07 -0300

    test: find a race in KnexRaceFinderRepository

--- src/infra/knex/repositories/race-finder/test.ts
+++ src/infra/knex/repositories/race-finder/test.ts
@@ -1,8 +1,41 @@
-import { mockRaceFinderRepositoryParams } from 'app/protocols/race-finder-repository/mock'
+import { RaceFinderRepositoryModel } from 'app/protocols/race-finder-repository'
+import {
+  mockRaceFinderRepositoryModel,
+  mockRaceFinderRepositoryParams,
+} from 'app/protocols/race-finder-repository/mock'
+import { KnexDbHandler } from 'infra/knex/config/knex-db-handler'
 import { Knex } from 'knex'
 import { KnexRaceFinderRepository } from '.'
 
+const makeSut = (client = KnexDbHandler.client) => {
+  const sut = new KnexRaceFinderRepository(client)
+
+  return {
+    sut,
+  }
+}
+
 describe('KnexRaceFinderRepository', () => {
+  let expectedRaceRepoModel: RaceFinderRepositoryModel
+
+  beforeAll(async () => {
+    KnexDbHandler.connect('test')
+    await KnexDbHandler.migrateLatest()
+
+    const raceToInsert = mockRaceFinderRepositoryModel()
+    delete raceToInsert.id
+
+    const races = await KnexDbHandler.client
+      .insert(raceToInsert, ['*'])
+      .into('races')
+
+    expectedRaceRepoModel = races[0]
+  })
+
+  afterAll(async () => {
+    await KnexDbHandler.disconnect()
+  })
+
   it('should call knex with right params', async () => {
     const client = {
       select: jest.fn().mockReturnThis(),
@@ -20,5 +53,13 @@ describe('KnexRaceFinderRepository', () => {
     expect(client.from).toBeCalledWith('races')
     expect(client.where).toBeCalledWith('id', params.id)
     expect(client.first).toBeCalled()
+  })
+
+  it('should find a race', async () => {
+    const { sut } = makeSut()
+
+    const raceRepoModel = await sut.find({ id: expectedRaceRepoModel.id })
+
+    expect(raceRepoModel).toEqual(expectedRaceRepoModel)
   })
 })
