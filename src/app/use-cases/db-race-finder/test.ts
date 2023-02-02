cfc0ed93a83dc4b890e25bef1be910df6d7465c0
Lucas Segundo <76710848+lucas-segundo@users.noreply.github.com>
2023-02-02 10:05:58 -0300

    test: return model data in DbRaceFinder

--- src/app/use-cases/db-race-finder/test.ts
+++ src/app/use-cases/db-race-finder/test.ts
@@ -1,15 +1,47 @@
-import { mockRaceFinderRepository } from 'app/protocols/race-finder-repository/mock'
+import {
+  mockRaceFinderRepository,
+  mockRaceFinderRepositoryModel,
+} from 'app/protocols/race-finder-repository/mock'
+import { Race } from 'domain/models/race'
 import { mockRaceFinderParams } from 'domain/use-cases/race-finder/mock'
 import { DbRaceFinder } from '.'
 
+const makeSut = () => {
+  const raceFinderRepo = mockRaceFinderRepository()
+  const sut = new DbRaceFinder(raceFinderRepo)
+
+  return {
+    sut,
+    raceFinderRepo,
+  }
+}
+
 describe('DbRaceFinder', () => {
   it('should call repo with right params', async () => {
-    const raceFinderRepo = mockRaceFinderRepository()
-    const sut = new DbRaceFinder(raceFinderRepo)
+    const { sut, raceFinderRepo } = makeSut()
+
+    const raceFinderRepoModel = mockRaceFinderRepositoryModel()
+    raceFinderRepo.find.mockResolvedValueOnce(raceFinderRepoModel)
 
     const params = mockRaceFinderParams()
     await sut.find(params)
 
     expect(raceFinderRepo.find).toBeCalledWith(params)
+  })
+
+  it('should return model data', async () => {
+    const { sut, raceFinderRepo } = makeSut()
+
+    const raceFinderRepoModel = mockRaceFinderRepositoryModel()
+    raceFinderRepo.find.mockResolvedValueOnce(raceFinderRepoModel)
+
+    const params = mockRaceFinderParams()
+    const race = await sut.find(params)
+
+    const expectedRace: Race = {
+      id: raceFinderRepoModel.id.toString(),
+      title: raceFinderRepoModel.title,
+    }
+    expect(race).toEqual(expectedRace)
   })
 })
